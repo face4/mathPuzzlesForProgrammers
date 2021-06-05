cf4b3aa22da04cf3e0372a8870876aa0f9d5b825
face4 <s2020583@u.tsukuba.ac.jp>
2021-06-05 17:21:05 +0900

    fix 05

--- q05.kt
+++ q05.kt
@@ -1,16 +1,15 @@
 import kotlin.math.min
 
-fun solve(n: Int, cpos: Int, coins: List<Int>, limit: Int): Int{
-    if(cpos == coins.size){
-        return if(n == 0) 1 else 0
-    }
-    var ret = 0
-    for(i in 0..min(n/coins[cpos], limit)){
-        ret += solve(n-coins[cpos]*i, cpos+1, coins, limit-i)
-    }
-    return ret
-}
-
 fun main() {
+    fun solve(n: Int, cpos: Int, coins: List<Int>, limit: Int): Int {
+        if (cpos == coins.size) {
+            return if (n == 0) 1 else 0
+        }
+        var ret = 0
+        for (i in 0..min(n / coins[cpos], limit)) {
+            ret += solve(n - coins[cpos] * i, cpos + 1, coins, limit - i)
+        }
+        return ret
+    }
     println(solve(1000, 0, listOf(500, 100, 50, 10), 15))
 }
